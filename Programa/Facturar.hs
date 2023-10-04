bd0ff3bf81f5f6c686e2d79e3be443e5c2601a27
Duan Antonio Espinoza Olivares <75913012+Duan-Espinoza@users.noreply.github.com>
2023-10-04 01:55:01 -0600

    arreglo
    se agrega una funcionalidad

--- Programa/Facturar.hs
+++ Programa/Facturar.hs
@@ -64,3 +64,38 @@ getTarifaPorKilometro (Factura _ _ _ _ _ _ _ t _) = t
 
 getMontoTotalFactura :: Factura -> MontoTotalFactura
 getMontoTotalFactura (Factura _ _ _ _ _ _ _ _ mT) = mT
+
+
+-- Muestra la información de una factura por su ID
+-- Entrada: lista de facturas, el ID de la factura a mostrar
+-- Salida: N/A
+mostrarFacturaPorID :: [Factura] -> IDFactura -> IO ()
+mostrarFacturaPorID [] codigoFactura = putStrLn "El código de factura ingresado no existe"
+mostrarFacturaPorID (factura:facturas) codigoFactura
+    | getIdentificadorFactura factura == codigoFactura = mostrarDetalleFactura factura
+    | otherwise = mostrarFacturaPorID facturas codigoFactura
+
+-- Función auxiliar para mostrar una factura
+mostrarDetalleFactura :: Factura -> IO ()
+mostrarDetalleFactura factura = do
+    let codigoFactura = getIdentificadorFactura factura
+        puntoSalida = getPuntoSalida factura
+        puntoDestino = getPuntoDestino factura
+        cedulaUsuario = getCedulaUsuarioFactura factura
+        codigoBicicleta = getCodigoBicicletaFactura factura
+        tipoBicicleta = getTipoBicicletaFactura factura
+        distanciaRecorrida = getDistanciaRecorrida factura
+        tarifaPorKilometro = getTarifaPorKilometro factura
+        montoTotalFactura = getMontoTotalFactura factura
+
+    putStrLn "Información de la factura:"
+    putStrLn $ "ID de Factura: " ++ show codigoFactura
+    putStrLn $ "Punto de Salida: " ++ puntoSalida
+    putStrLn $ "Punto de Destino: " ++ puntoDestino
+    putStrLn $ "Cédula de Usuario: " ++ show cedulaUsuario
+    putStrLn $ "Código de Bicicleta: " ++ codigoBicicleta
+    putStrLn $ "Tipo de Bicicleta: " ++ tipoBicicleta
+    putStrLn $ "Distancia Recorrida (en kilómetros): " ++ show distanciaRecorrida
+    putStrLn $ "Tarifa por Kilómetro: " ++ show tarifaPorKilometro
+    putStrLn $ "Monto Total de la Factura: " ++ show montoTotalFactura
+    putStrLn "--------------------------------------"
